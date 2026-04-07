fa51c5200664dc655574b1be7572d7926614f7d6
Om Ra <me@xomrkob.ru>
2026-04-07 16:03:44 +0600

    fix error invalidate cache

--- src/store/middleware/socketMiddleware.ts
+++ src/store/middleware/socketMiddleware.ts
@@ -18,12 +18,16 @@ export const socketMiddleware: Middleware<{}, PartialRootState> = (store) => {
         `wss://api.example.com/stream/ws/updates?token=${token}`,
       );
       socket.onmessage = (event) => {
+        console.log("message from back");
         try {
           const data = JSON.parse(event.data);
+          console.log("received message", data);
           if (data.type === "STREAM_UPDATED") {
-            streamApi.util.invalidateTags([
-              { type: "Stream", id: data.payload?.stream_id },
-            ]);
+            store.dispatch(
+              streamApi.util.invalidateTags([
+                { type: "Stream", id: data.payload?.stream_id },
+              ]),
+            );
           }
         } catch (e) {
           console.error("WS parse error", e);
